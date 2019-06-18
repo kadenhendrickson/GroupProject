f7d5b87de11ceac74469c572c59fb6ad3877307d
Kaden Hendrickson <kadenhendrickson4@gmail.com>
2019-06-18 10:26:35 -0600

    Model and ModelController files added

---
+++ TeamNightHawkGroupProject/ModelControllers/RecipeController.swift
@@ -0,0 +1,9 @@
+//
+//  RecipeController.swift
+//  TeamNightHawkGroupProject
+//
+//  Created by Kaden Hendrickson on 6/18/19.
+//  Copyright © 2019 DevMountain. All rights reserved.
+//
+
+import Foundation
