37b0620f4d76b9f510a1da4639d9000d0cfeda1b
srussellh <shanerherbert@gmail.com>
2019-06-19 15:26:45 -0600

    updated detailTVC

--- TeamNightHawkGroupProject/ViewControllers/RecipeDetailTableViewController.swift
+++ TeamNightHawkGroupProject/ViewControllers/RecipeDetailTableViewController.swift
@@ -9,19 +9,50 @@
 import UIKit
 
 class RecipeDetailTableViewController: UITableViewController {
+    @IBOutlet weak var userImage: UIImageView!
+    @IBOutlet weak var userNameLabel: UIButton!
+    @IBOutlet weak var recipeImage: UIButton!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var servingsLabel: UILabel!
+    @IBOutlet weak var prepTimeLabel: UILabel!
+    
     
     var recipe: Recipe?
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        guard let recipe = recipe,
+        let imageData = recipe.image,
+        let user = UserController.shared.users[recipe.userReference] else {return}
+        userImage.image = UIImage(data: imageData)
+        userNameLabel.setTitle(user.displayName, for: .normal)
+        
+        
     }
+    
+    @IBAction func backButtonTapped(_ sender: Any) {
+        navigationController?.popViewController(animated: true)
+    }
+    
+
     
     // MARK: - Table view data source
     override func numberOfSections(in tableView: UITableView) -> Int {
+        
         return 2
     }
     
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == 0 {
+            return "Ingredients"
+        }
+        return "Directions"
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
         guard let recipe = recipe else {return 0}
         if section == 0{
             return recipe.ingredients.count
@@ -36,7 +67,7 @@ class RecipeDetailTableViewController: UITableViewController {
         
         // Configure the cell...
         if indexPath.section == 0{
-            guard let cell = tableView.dequeueReusableCell(withIdentifier: "RecipeCell", for: indexPath) as? Section1TableViewCell,
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "RecipeCell", for: indexPath) as? IngredientsTableViewCell,
                 let recipe = recipe else {return UITableViewCell()}
             
             cell.measurementQuantity.text = recipe.ingredients[indexPath.row].measurementQuantity
@@ -45,7 +76,7 @@ class RecipeDetailTableViewController: UITableViewController {
             
             return cell
         }else{
-            guard let cell = tableView.dequeueReusableCell(withIdentifier: "RecipeCell", for: indexPath) as? Section2TableViewCell,
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "RecipeCell", for: indexPath) as? StepsTableViewCell,
                 let recipe = recipe,
                 let steps = recipe.steps else {return UITableViewCell()}
             
