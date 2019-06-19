a3970b0029ecb07fc9c4b7cb8d4162c8a85df72b
srussellh <shanerherbert@gmail.com>
2019-06-19 11:58:48 -0600

    section 2

--- TeamNightHawkGroupProject/Section2TableViewCell.swift
+++ TeamNightHawkGroupProject/Section2TableViewCell.swift
@@ -9,16 +9,55 @@
 import UIKit
 
 class Section2TableViewCell: UITableViewCell {
-
+    
+    var safeArea: UILayoutGuide {
+        return self.safeAreaLayoutGuide
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        setUpStackView()
+        addAllSubViews()
+        
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    func addAllSubViews(){
+        self.addSubview(directionSteps)
+        self.addSubview(addSection)
+        self.addSubview(stackView)
+        
     }
-
+    
+    func setUpStackView(){
+        stackView.addArrangedSubview(directionSteps)
+        stackView.addArrangedSubview(addSection)
+        stackView.anchor(top: safeArea.topAnchor, bottom: safeArea.bottomAnchor, leading: safeArea.leadingAnchor, trailing: safeArea.trailingAnchor, paddingTop: 0, paddingBottom: 0, paddingLeading: 0, paddingTrailing: 0, height: (fontSize + 8))
+        
+    }
+    
+    let directionSteps: UITextField = {
+        let text = UITextField()
+        text.font = UIFont(name: fontName, size: fontSize)
+        
+        return text
+    }()
+    
+    let addSection: UIButton = {
+        let button = UIButton()
+        button.setTitleColor(addSectionButtonColor, for: .normal)
+        button.setTitle("+", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: addSectionButtonSize)
+        button.contentHorizontalAlignment = .center
+        button.layer.cornerRadius = addSectionButtonCorner
+        
+        return button
+    }()
+    
+    let stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.alignment = .fill
+        stackView.distribution = .fillProportionally
+        stackView.spacing = 8
+        return stackView
+    }()
 }
