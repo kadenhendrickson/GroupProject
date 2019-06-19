73be0c4eaa4cfac7764a046f287c0fdb3ed34f71
srussellh <shanerherbert@gmail.com>
2019-06-19 11:57:47 -0600

    adds extension to view

--- TeamNightHawkGroupProject/Section1TableViewCell.swift
+++ TeamNightHawkGroupProject/Section1TableViewCell.swift
@@ -8,17 +8,79 @@
 
 import UIKit
 
-class Section1TableViewCell: UITableViewCell {
+//Need Function to tap button
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        
-        
+class Section1TableViewCell: UITableViewCell {
+    
+    var safeArea: UILayoutGuide {
+        return self.safeAreaLayoutGuide
     }
     
-    var saveButton: UIButton {
-        let button = UIButton()
-        button.setTitle("+", for: .normal)
-        return button
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        addAllSubViews()
+        setUpStackView()
     }
+    
+    func addAllSubViews(){
+        self.addSubview(measuremenType)
+        self.addSubview(measurementQuantity)
+        self.addSubview(ingredient)
+        self.addSubview(addSection)
+        self.addSubview(stackView)
+    }
+    
+    
+    
+    func setUpStackView(){
+        stackView.addArrangedSubview(measurementQuantity)
+        stackView.addArrangedSubview(measuremenType)
+        stackView.addArrangedSubview(ingredient)
+        stackView.addArrangedSubview(addSection)
+        stackView.anchor(top: safeArea.topAnchor, bottom: safeArea.bottomAnchor, leading: safeArea.leadingAnchor, trailing: safeArea.trailingAnchor, paddingTop: 0, paddingBottom: 0, paddingLeading: 0, paddingTrailing: 0, height: (fontSize + 8))
+    }
+    
+    
+    let measurementQuantity: UITextField = {
+        let text = UITextField()
+        text.font = UIFont(name: fontName, size: fontSize)
+        
+        return text
+    }()
+    
+    let measuremenType: UITextField = {
+        let text = UITextField()
+        text.font = UIFont(name: fontName, size: fontSize)
+        
+        return text
+    }()
+    
+    let ingredient: UITextField = {
+        let text = UITextField()
+        text.font = UIFont(name: fontName, size: fontSize)
+        
+        return text
+    }()
+    
+    
+    let addSection: UIButton = {
+        let button = UIButton()
+        button.setTitleColor(addSectionButtonColor, for: .normal)
+        button.setTitle("+", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: addSectionButtonSize)
+        button.contentHorizontalAlignment = .center
+        button.layer.cornerRadius = addSectionButtonCorner
+        
+        return button
+    }()
+    
+    
+    let stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.alignment = .fill
+        stackView.distribution = .fillProportionally
+        stackView.spacing = 8
+        return stackView
+    }()
 }
