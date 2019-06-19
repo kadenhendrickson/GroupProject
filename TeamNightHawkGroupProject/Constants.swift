9c40bce93a0b73f72dbac076b5db44d02d647c35
srussellh <shanerherbert@gmail.com>
2019-06-19 11:55:09 -0600

    changes files

---
+++ TeamNightHawkGroupProject/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  TeamNightHawkGroupProject
+//
+//  Created by Bobba Kadush on 6/19/19.
+//  Copyright © 2019 DevMountain. All rights reserved.
+//
+
+import Foundation
---
+++ TeamNightHawkGroupProject/Section2TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  Section2TableViewCell.swift
+//  TeamNightHawkGroupProject
+//
+//  Created by Bobba Kadush on 6/19/19.
+//  Copyright © 2019 DevMountain. All rights reserved.
+//
+
+import UIKit
+
+class Section2TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
