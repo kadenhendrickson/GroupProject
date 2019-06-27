a8a554d7bee7f3180a92bc778bc188853f0658ff
Kaden Hendrickson <kadenhendrickson4@gmail.com>
2019-06-27 11:38:26 -0600

    finalizes userFeed functionality

--- TeamNightHawkGroupProject/ViewControllers/UserFeedTableViewController.swift
+++ TeamNightHawkGroupProject/ViewControllers/UserFeedTableViewController.swift
@@ -11,82 +11,86 @@ import FirebaseAuth
 
 class UserFeedTableViewController: UITableViewController, UserFeedTableViewCellDelegate {
     //MARK: - Properties
-    var recipesList: [Recipe] = {
-        guard let currentUser = UserController.shared.currentUser else {return []}
-        var internalRecipes: [Recipe] = []
-        UserController.shared.db.collection("Users").document(currentUser.userID).getDocument { (snapshot, error) in
-            if let error = error {
-                print("there is an error fetching users: \(error.localizedDescription)")
-            }
-            guard let data = snapshot?.data(),
-                    let friendsArray = data["followingRefs"] as? [String] else {return}
-            
-            for friend in friendsArray {
-                RecipeController.shared.fetchSpecificRecipesWith(userReference: friend, completion: { (recipes) in
-                    internalRecipes.append(contentsOf: recipes)
-                })
-            }
+    var recipesList: [Recipe]? {
+        didSet{
+            print("Recipe Count: \(self.recipesList?.count)")
+            tableView.reloadData()
         }
-        return internalRecipes
-    }()
-    
-    private var handle: AuthStateDidChangeListenerHandle?
-    
-    //properties
-    var usersList: [User] = {
-        var users: [User] = []
-        guard let currentUserFollowingRefs = UserController.shared.currentUser?.followingRefs else {return []}
-        for userRef in currentUserFollowingRefs {
-            UserController.shared.fetchUser(withUserRef: userRef) { (user) in
-                users.append(user)
-            }
-        }
-        return users
-    }()
+    }
+    var usersList: [User]?
     
     var selectedUser: User?
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        loadUsers()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        tableView.reloadData()
+    }
+    
+    //helper functions
+    func loadUsers() {
+        usersList = []
+        recipesList = []
+        guard let currentUserFollowingRefs = UserController.shared.currentUser?.followingRefs else {return}
+        for userRef in currentUserFollowingRefs {
+            UserController.shared.fetchUser(withUserRef: userRef) { (user) in
+                self.usersList?.append(user)
+                RecipeController.shared.fetchSpecificRecipesWith(userReference: userRef, completion: { (recipes) in
+                    self.recipesList?.append(contentsOf: recipes)
+                })
+            }
+        }
+    }
+    
+    func findUserForRecipe(with recipe: Recipe) -> User? {
+        guard let usersList = usersList else {return nil}
+        for user in usersList {
+            if recipe.userReference == user.userID {
+                return user
+            }
+        }
+        return nil
     }
     
     // MARK: - Table view data source
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return recipesList.count
+        guard let numberOfRows = recipesList?.count else {return 0}
+        print("\(numberOfRows)")
+        return numberOfRows
     }
     
     //dont forget to change reuseIdentifier
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "userFeedRecipeCell", for: indexPath) as? UserFeedTableViewCell
+        guard let recipe = recipesList?[indexPath.row] else {return UITableViewCell()}
         cell?.delegate = self
-        cell?.recipe = recipesList[indexPath.row]
-        cell?.user = usersList[indexPath.row]
+        cell?.recipe = recipe
+        cell?.user = findUserForRecipe(with: recipe)
         return cell ?? UITableViewCell()
     }
-
+    
     // MARK: - Navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "fromFeedToRecipeDVC" {
             guard let destinationVC = segue.destination as? RecipeDetailTableViewController,
                 let indexPath = tableView.indexPathForSelectedRow else {return}
-            let recipe = recipesList[indexPath.row]
+            let recipe = recipesList?[indexPath.row]
             destinationVC.recipe = recipe
         }
         if segue.identifier == "fromFeedToOtherUserVC" {
             guard let destinationVC = segue.destination as? UserViewedTableViewController else {return}
             destinationVC.user = selectedUser
         }
-}
+    }
     
     func userRefSent(userRef: String) {
         UserController.shared.fetchUser(withUserRef: userRef) { (user) in
             self.selectedUser = user
         }
     }
+    
+    
 }
