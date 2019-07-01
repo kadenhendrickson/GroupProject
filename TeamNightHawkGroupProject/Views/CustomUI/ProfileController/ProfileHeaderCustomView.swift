f721cec4538050efb5d7577c538613625ff08dae
Annicha <annicha.dev@gmail.com>
2019-07-01 16:18:54 -0600

    Added files

---
+++ TeamNightHawkGroupProject/Views/CustomUI/ProfileController/ProfileHeaderCustomView.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileHeaderCustomView.swift
+//  TeamNightHawkGroupProject
+//
+//  Created by Annicha Hanwilai on 7/1/19.
+//  Copyright © 2019 DevMountain. All rights reserved.
+//
+
+import UIKit
+
+class ProfileHeaderCustomView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
