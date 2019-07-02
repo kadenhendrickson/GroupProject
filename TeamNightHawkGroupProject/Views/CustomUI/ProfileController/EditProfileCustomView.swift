3ca8bc227ab927b13cb5bb0fde7123c2e88275ce
Annicha <annicha.dev@gmail.com>
2019-07-02 13:21:21 -0600

    Inplement edit profile view custom colors.

--- TeamNightHawkGroupProject/Views/CustomUI/ProfileController/EditProfileCustomView.swift
+++ TeamNightHawkGroupProject/Views/CustomUI/ProfileController/EditProfileCustomView.swift
@@ -8,14 +8,60 @@
 
 import UIKit
 
-class EditProfileCustomView: UIView {
+struct EditProfileConstants {
+    // Buttons
+    static let buttonBorderRadius = textFieldRounding
+    static let borderWidth = CGFloat(0.8)
+    
+    // TextFields
+    static let greyBorderColor = grey.cgColor
+    static let buttonColor = green
+}
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+class editTextView: UITextView {
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.font = UIFont(name: fontName, size: fontSize)
+        self.layer.borderWidth = EditProfileConstants.borderWidth
+        self.layer.borderColor = EditProfileConstants.greyBorderColor
+        self.layer.cornerRadius = textFieldRounding
     }
-    */
+}
 
+class displayNameTextField: UITextField {
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.font = UIFont(name: fontName, size: fontSize)
+        self.layer.borderWidth = EditProfileConstants.borderWidth
+        self.layer.borderColor = EditProfileConstants.greyBorderColor
+        self.layer.cornerRadius = textFieldRounding
+    }
+}
+
+class editProfileButton: UIButton {
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.setTitleColor(white, for: .normal)
+        self.backgroundColor = EditProfileConstants.buttonColor
+        self.heightAnchor.constraint(equalToConstant: saveButtonSize)
+        self.layer.cornerRadius = EditProfileConstants.buttonBorderRadius
+    }
+}
+
+class deleteProfileButton: UIButton {
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.setTitleColor(white, for: .normal)
+        self.backgroundColor = UIColor.red
+        self.heightAnchor.constraint(equalToConstant: saveButtonSize)
+        self.layer.cornerRadius = EditProfileConstants.buttonBorderRadius
+    }
+}
+
+class ProfilePicture: UIImageView {
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.layer.cornerRadius = 50
+        self.clipsToBounds = true
+    }
 }
