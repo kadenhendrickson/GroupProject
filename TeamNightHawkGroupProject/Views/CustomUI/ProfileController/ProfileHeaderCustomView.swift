a87cc263a9a2218ff71912b3aaf4fecaeab9fe62
Annicha <annicha.dev@gmail.com>
2019-07-02 15:29:24 -0600

    Implement aligntop for label

--- TeamNightHawkGroupProject/Views/CustomUI/ProfileController/ProfileHeaderCustomView.swift
+++ TeamNightHawkGroupProject/Views/CustomUI/ProfileController/ProfileHeaderCustomView.swift
@@ -20,22 +20,73 @@ struct ProfileHeaderConstants {
     static let buttonColor = green
 }
 
-
-class ProfileButton: UIButton {
+class DisplayNameLabel: UILabel {
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        self.setTitleColor(white, for: .normal)
-        self.backgroundColor = ProfileHeaderConstants.buttonColor
-        self.heightAnchor.constraint(equalToConstant: saveButtonSize)
-        self.layer.cornerRadius = EditProfileConstants.buttonBorderRadius
+        self.font = UIFont(name: fontName, size: fontSize)
     }
 }
 
-class ProfilePictureInDetailView: UIImageView {
+class BioTextLabel: UILabel {
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        self.layer.cornerRadius = (self.frame.height/2)
+        self.font = UIFont(name: fontName, size: fontSize)
+        self.layer.borderColor = softBlue.cgColor
+        self.layer.borderWidth = ProfileHeaderConstants.borderWidth
+        self.layer.cornerRadius = ProfileHeaderConstants.buttonBorderRadius
+    }
+    
+    enum VerticalAlignment {
+        case top
+        case middle
+        case bottom
+    }
+    
+    var verticalAlignment : VerticalAlignment = .top {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+    
+    override public func textRect(forBounds bounds: CGRect, limitedToNumberOfLines: Int) -> CGRect {
+        let rect = super.textRect(forBounds: bounds, limitedToNumberOfLines: limitedToNumberOfLines)
+        
+        if UIView.userInterfaceLayoutDirection(for: .unspecified) == .rightToLeft {
+            switch verticalAlignment {
+            case .top:
+                return CGRect(x: self.bounds.size.width - rect.size.width, y: bounds.origin.y, width: rect.size.width, height: rect.size.height)
+            case .middle:
+                return CGRect(x: self.bounds.size.width - rect.size.width, y: bounds.origin.y + (bounds.size.height - rect.size.height) / 2, width: rect.size.width, height: rect.size.height)
+            case .bottom:
+                return CGRect(x: self.bounds.size.width - rect.size.width, y: bounds.origin.y + (bounds.size.height - rect.size.height), width: rect.size.width, height: rect.size.height)
+            }
+        } else {
+            switch verticalAlignment {
+            case .top:
+                return CGRect(x: bounds.origin.x, y: bounds.origin.y, width: rect.size.width, height: rect.size.height)
+            case .middle:
+                return CGRect(x: bounds.origin.x, y: bounds.origin.y + (bounds.size.height - rect.size.height) / 2, width: rect.size.width, height: rect.size.height)
+            case .bottom:
+                return CGRect(x: bounds.origin.x, y: bounds.origin.y + (bounds.size.height - rect.size.height), width: rect.size.width, height: rect.size.height)
+            }
+        }
+    }
+}
+
+class MainProfileHeaderButton: UIButton {
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.setTitleColor(black, for: .normal)
+        self.backgroundColor = ProfileHeaderConstants.buttonColor
+        self.heightAnchor.constraint(equalToConstant: saveButtonSize)
+        self.layer.cornerRadius = ProfileHeaderConstants.buttonBorderRadius
+    }
+}
+
+class ProfilePicture: UIImageView {
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.layer.cornerRadius = 50
         self.clipsToBounds = true
     }
 }
-
