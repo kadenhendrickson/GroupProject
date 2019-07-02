0fa9b32c21fa4123e3d9ae892476739ae5cbf612
Kaden Hendrickson <kadenhendrickson4@gmail.com>
2019-07-02 11:42:37 -0600

    new users can unfollow OG's and user feed now contains current users posts

---
+++ TeamNightHawkGroupProject/Views/CustomUI/RecipeSaveButton.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeSaveButton.swift
+//  TeamNightHawkGroupProject
+//
+//  Created by Kaden Hendrickson on 7/2/19.
+//  Copyright © 2019 DevMountain. All rights reserved.
+//
+
+import UIKit
+
+class RecipeSaveButton: UIButton {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+}
